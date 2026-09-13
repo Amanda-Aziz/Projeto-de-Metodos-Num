c71099988fcd481337bcc9f8e9a66f297b290bff
FrancisLauriano <francislauriano@gmail.com>
2026-09-13 11:38:10 -0300

    orientaçoes para implementacao do algoritimo de cada metodo

---
+++ src/metodos_numericos/secante.py
@@ -0,0 +1,44 @@
+# ORIENTAÇÃO DE RETORNO - MÉTODO DA SECANTE
+#
+# A implementação do método fica a critério do responsável.
+# O importante é manter o retorno compatível com o notebook.
+#
+# O método deve retornar um dicionário contendo:
+#
+# "raiz":
+#     Aproximação final obtida para a raiz.
+#
+# "convergiu":
+#     True se o método atender aos critérios de convergência.
+#     False caso contrário.
+#
+# "iteracoes":
+#     Quantidade de iterações realizadas.
+#
+# "erro":
+#     Erro entre aproximações consecutivas.
+#
+# "residuo":
+#     Valor absoluto de f(x) na aproximação final.
+#
+# "historico":
+#     Lista com os dados de todas as iterações.
+#
+# Cada elemento do histórico deve conter, no mínimo:
+#
+# {
+#     "iteracao": ...,
+#     "x_anterior": ...,
+#     "x_atual": ...,
+#     "erro": ...,
+#     "residuo": ...
+# }
+#
+# Para compatibilidade com o notebook, "x_atual" deve
+# representar a aproximação obtida naquela iteração.
+#
+# Importante: os arquivos em src/metodos_numericos/ 
+# devem implementar somente os algoritmos genéricos. 
+# Não colocar neles g, L, t, v_alvo, f(H), phi(H), 
+# derivada_f(H), [1,2] ou H0=1.5, 
+# porque essas informações pertencem à aplicação do problema e ficam no notebook.
